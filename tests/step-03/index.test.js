8ead7b83c5337eb67951d91da187f0f140abfc57
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-04-02 23:17:41 +0530

    feat: add step-8

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,25 +1,14 @@
-const readCSV = require("../../src/csvReader");
-const parseQuery = require("../../src/queryParser");
-
-test("Read CSV File", async () => {
-  const data = await readCSV("./sample.csv");
-  expect(data.length).toBeGreaterThan(0);
-  expect(data.length).toBe(3);
-  expect(data[0].name).toBe("John");
-  expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
-});
+const { parseQuery } = require("../../src/queryParser");
 
 test("Parse SQL Query", () => {
-  const query = "SELECT id, name FROM sample";
+  const query = "SELECT id, name FROM student";
   const parsed = parseQuery(query);
   expect(parsed).toEqual({
     fields: ["id", "name"],
-    table: "sample",
-    whereClause: null,
+    table: "student",
+    whereClauses: [],
+    joinCondition: null,
+    joinTable: null,
+    joinType: null,
   });
-});
-
-test("Parse SQL Query with Invalid Format", () => {
-  const query = "Invalid SQL query";
-  expect(() => parseQuery(query)).toThrow("Invalid query format");
 });
